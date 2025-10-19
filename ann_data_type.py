60a8e547d1b42b5593d88dd9ea3e60e191786bce
imamputra1 <a.i.syahputra415@gmail.com>
2025-10-20 06:18:08 +0700

    add data type topic

---
+++ ann_data_type.py
@@ -0,0 +1,22 @@
+# -- tipe data Numerik --
+bilangan_bulat: int = 18
+bilangan_desimal: float = 1.82
+bilangan_kompleks: complex = 10j
+
+
+tipedata_boolean: bool = True
+tipedata_strings: str = "strings"
+tipedata_list: list = [1, 1.2, 12j, "satu", True,]
+tipedata_tuple: tuple = (1, 1.2, 12j, "satu", True,)
+tipedata_set: set = {"hashable", 18}
+tipedata_dict: dict = {"hashable": 18}
+                       
+print(bilangan_bulat,type(bilangan_bulat))
+print(bilangan_desimal,type(bilangan_desimal))
+print(bilangan_kompleks,type(bilangan_kompleks))
+print(tipedata_boolean,type(tipedata_boolean))
+print(tipedata_strings,type(tipedata_strings))
+print(tipedata_list,type(tipedata_list))
+print(tipedata_tuple,type(tipedata_tuple))
+print(tipedata_set,type(tipedata_set))
+print(tipedata_dict,type(tipedata_dict))
